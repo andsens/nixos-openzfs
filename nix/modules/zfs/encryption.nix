75c58485fd348c3547ad89ed682846bd9a9ebba9
Anders Ingemann <anders@ingemann.de>
2026-05-18 20:21:08 +0200

    Fix PATH when encrypting ZFS key

--- nix/modules/zfs/encryption.nix
+++ nix/modules/zfs/encryption.nix
@@ -44,12 +44,18 @@ in
         requires = [ "ZFS_ENCRYPTION_KEY_${pool}" ];
         cmd = lib.getExe (
           pkgs.writeShellScriptBin "zfs-encrypt-key-tpm2.sh" ''
+            PATH=${
+              pkgs.lib.makeBinPath [
+                pkgs.coreutils
+                pkgs.systemd
+              ]
+            }
             umask 700
             keyfile=$(mktemp --suffix zfs-key)
             trap "rm -f \"$keyfile\"" EXIT
             printf "%s" "$ZFS_ENCRYPTION_KEY_${pool}" >"$keyfile"
             mkdir -p /etc/credstore.encrypted/
-            ${lib.getExe' pkgs.systemd "systemd-creds"} encrypt --tpm2-device=auto --tpm2-pcrs=${lib.join "+" (map builtins.toString spec.tpmPCRs)} "$keyfile" "/etc/credstore.encrypted/${pool}.zfs-key"
+            systemd-creds encrypt --tpm2-device=auto --tpm2-pcrs=${lib.join "+" (map builtins.toString spec.tpmPCRs)} "$keyfile" "/etc/credstore.encrypted/${pool}.zfs-key"
           ''
         );
       }) autoDecryptPools;
